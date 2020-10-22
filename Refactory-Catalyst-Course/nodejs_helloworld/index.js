5b60bbdfecbb6c3f6e0e14c7118b109e6018ef4e
LizKamugisha <liz.kamugisha@gmail.com>
2020-10-22 10:20:58 +0300

    Add practice work on creating servers with node.js

--- Refactory-Catalyst-Course/nodejs_helloworld/index.js
+++ Refactory-Catalyst-Course/nodejs_helloworld/index.js
@@ -1,1 +1,24 @@
-console.log('Hello, World! Node is working...');
+//To test if the node is working
+// console.log('Hello, World! Node is working...');
+
+//In order to use the express dependencies we need to import it into our project using require key word 
+const express = require('express')
+//create an express application by calling the express() function
+const app = express();
+
+// Specify what to do when user hits the '/' (home page) route/endpoint
+// eg. res.send('Homepage ! Hello World.')
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/helloWorld.html')
+});
+
+// Specify what to do when user hits the '/about' (home page) route/endpoint
+// app.get('/about', (req,res)=>{
+//     res.send("This is the about us page")
+// })
+
+//Specify what to do when
+
+// Create a server that listens on port 3000 for requests
+app.listen(3000, ()=> console.log('listening on port 3000'))
---
+++ Refactory-Catalyst-Course/simpleServer/server.js
@@ -0,0 +1,23 @@
+// To check if server is running well
+// console.log ('pdp pdp pdp pdp')
+
+const express = require('express');
+const bodyParser=require('body-parser')
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}))
+
+// Connect browser to server using listen method
+app.listen(3000,function(){
+    console.log('listening on 3000')
+})
+// Connecting server to html page
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname+'/index.html')
+});
+
+app.post('/quotes', (req, res) => { 
+    console.log(req.body)
+  })
+
+
+
